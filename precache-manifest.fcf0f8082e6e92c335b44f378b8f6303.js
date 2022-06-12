01646aca8974cc21531f441f9ea3db3e3122bca8
rinat.idiiatov <rinat.idiiatov@qlever.com>
2022-06-13 01:47:57 +0300

    deploy

---
+++ precache-manifest.fcf0f8082e6e92c335b44f378b8f6303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a0bbecc8da9f10effb",
+    "url": "/game_of_good-frontend-/css/app.9ac01acf.css"
+  },
+  {
+    "revision": "a10df6cb0ed2c7b2ae6f",
+    "url": "/game_of_good-frontend-/css/chunk-vendors.3c6efc88.css"
+  },
+  {
+    "revision": "28796cd0cd85d58f7f65b90cc3a0c4f7",
+    "url": "/game_of_good-frontend-/index.html"
+  },
+  {
+    "revision": "19a0bbecc8da9f10effb",
+    "url": "/game_of_good-frontend-/js/app.35f0affc.js"
+  },
+  {
+    "revision": "a10df6cb0ed2c7b2ae6f",
+    "url": "/game_of_good-frontend-/js/chunk-vendors.dd3d0e79.js"
+  },
+  {
+    "revision": "b838aa4273e0538cec2e913e4b24d973",
+    "url": "/game_of_good-frontend-/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/game_of_good-frontend-/robots.txt"
+  }
+]);
